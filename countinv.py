498572218ca7dc00acaeaacebc746d2a75e8e5b0
Gil Rutter <g.o.rutter@warwick.ac.uk>
2015-10-05 18:26:52 +0100

    Take file as input and use numpy array instead of python list.

--- countinv.py
+++ countinv.py
@@ -4,6 +4,8 @@
 #Discussed in section 3.
 
 import sys
+import argparse
+import numpy as np
 
 def sortAndCountInv(A):
     """Count inversions in an n-length array in O(n log n) time using mergesort
@@ -15,7 +17,7 @@ def sortAndCountInv(A):
 
     lenA = len(A)
 
-    if len(A) == 1:
+    if lenA == 1:
         return A, 0
 
     else:
@@ -24,34 +26,44 @@ def sortAndCountInv(A):
 
         #Merge sorted arrays B and C
         #indexes of lists to iterate over:
-        i = 0 
+        i = 0
         j = 0
         #inversion counter:
         z = 0
 
-        sortedA = []
+        sortedA = np.array([], dtype=int)
 
         while True:
             if B[i] <= C[j]:
-                sortedA.append(B[i])
+                sortedA = np.append(sortedA, B[i])
                 i += 1
             else:
-                sortedA.append(C[j])
+                sortedA = np.append(sortedA, C[j])
                 z += ( len(B)-i )
                 j += 1
 
             #If an array reaches its end, add on all the elements from other
             if i == len(B):
-                return sortedA + C[j:], x+y+z
+                return np.append(sortedA, C[j:]), x+y+z
             elif j == len(C):
-                return sortedA + B[i:], x+y+z
+                return np.append(sortedA, B[i:]), x+y+z
 
 if __name__ == "__main__":
 
-    try:
-        array = [ float(element) for element in sys.argv[1:] ]
-    except ValueError:
-        print "Please pass an unsorted list of floats as the arguments of the program."
-        exit()
+    parser = argparse.ArgumentParser(
+        'Use mergeSort logic to count inversions in an array of ints. O(n log n)')
+    parser.add_argument('-A', type=float, nargs='+',
+            help='Array to sort as command-line input')
+    parser.add_argument('-f', type=str,
+            help='Order of statistic to return')
+    args = parser.parse_args()
 
-    print sortAndCountInv(array)
+    if args.f != None:
+        A = np.fromfile(args.f, sep=" ", dtype=int)
+    elif args.A != None:
+        A = np.array(args.A, dtype=int)
+    else:
+        print "Please pass an array in the command line after -A, or the name"\
+              " of a file containing an array after -f."
+        exit()
+    print sortAndCountInv(A)[1]
